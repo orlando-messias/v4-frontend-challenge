28cf5531c27f72ed463c10a943e3b3dfd5c31c2b
orlando-messias <programadorlando@gmail.com>
2020-12-31 01:26:46 -0300

    Tool item component + css

---
+++ src/components/ToolItem/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { Tool } from '../../pages';
+import { TiDelete, FaTrashAlt } from 'react-icons/all';
+import './style.css';
+
+interface ToolProps {
+  tool: Tool;
+}
+
+const ToolItem: React.FC<ToolProps> = ({ tool }) => {
+  return (
+    <div className="tool-item">
+      <div className="head">
+        <span className="title"><a href={tool.link}>{tool.title}</a></span>
+        <div className="buttons">
+          <FaTrashAlt className="delete-icon" />
+          <span>remove</span>
+        </div>
+      </div>
+      <div className="body">
+        <p className="description">{tool.description}</p>
+        <p>{tool.tags.map(tag => (
+          <span className="tag">{`#${tag}`}</span>
+        ))}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ToolItem;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,36 @@
 import React, { useEffect, useState } from 'react';
-import api from '../services/api';
 
-interface Tool {
+import ToolItem from '../components/ToolItem';
+import api from '../services/api';
+import logo from '../images/v4logo.png';
+import './style.css';
+
+export interface Tool {
   title: string;
   link: string;
   description: string;
+  tags: string[];
 };
 
 function Home() {
   const [tools, setTools] = useState<Tool[]>([]);
 
   useEffect(() => {
-    api.get<Tool[]>('/tools').then(response => {
+    api.get<Tool[]>('/tools?tag=online').then(response => {
       setTools(response.data);
     });
   });
 
   return (
-    <div>
-      <h2>This is Home</h2>
-      {tools.map(tool => <p>{tool.title}</p>)}
+    <div className="container">
+      <header className="header">
+        <img src={logo} alt="logo v4" />
+        <div className="title">
+          <h1>VUTTR</h1>
+          <p>Very Useful Tools to Remember</p>
+        </div>
+      </header>
+      {tools.map(tool => <ToolItem tool={tool} />)}
     </div>
   )
 }
