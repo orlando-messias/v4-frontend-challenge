bba22071b73ab4ef40ef766c87eba7ddfe8666d2
orlando-messias <programadorlando@gmail.com>
2021-01-01 12:33:15 -0300

    search bar onchange functionality

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 
 import { Container, AddButton, SearchInput, CheckBoxInput } from './style';
 import { FaPlus, FaSearch, FaUserCircle } from "react-icons/fa";
+import api from '../../services/api';
 
 interface SearchBarProps {
   setShowModal: Function;
 }
 
 export const SearchBar: React.FC<SearchBarProps> = ({ setShowModal }) => {
+  const [tools, setTools] = useState('');
+
+  async function searchTool(q: string) {
+    await api.get(`/tools?tag=${q}`).then(response => {
+      setTools(response.data);
+      console.log(tools);
+    });
+  };
 
   return (
     <Container>
       <div className="inputs">
         <div className="searchBar">
           <FaSearch id='faSearch' />
-          <SearchInput placeholder='search tool' />
+          <SearchInput placeholder='search tool' onChange={(e) => searchTool(e.target.value)} />
         </div>
         <div className="searchCheckContainer">
           <div className="searchCheck" >
